683ea9991ee5fcce82b9c3a6b2e910ae23fc3b5b
Deya Berger <deyaberger@MacBook-Air-de-Deya.local>
2022-01-26 23:28:31 +0100

    on y est presque

--- src/form/form.js
+++ src/form/form.js
@@ -1,1 +1,31 @@
-import "./form.css"
+import "./form.css";
+
+document.getElementsByTagName("a").addEventListener("click", function() {
+	console.log("form.gpu = ", form.gpu);
+});
+
+
+
+var input_list = document.querySelectorAll(".user-box > input");
+console.log(input_list);
+
+var gpu = false;
+
+if (input_list)
+{
+	input_list[0].addEventListener("click", function() {
+		// 	var form = new Form(false);
+		// 	console.log("form.gpu = ", form.gpu);
+		});
+}
+
+if (document.title == 'Computer')
+{
+	
+	console.log("You want to play against the computer");
+}
+else if (document.title == 'Humans')
+{
+	console.log("You want to play against a human");
+}
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./accueil.css";
+import Form from "./form/form";
 
 
 // document.querySelector(".computer > button").addEventListener("click", function() {
-// 	location.replace("./computer.html");
-//   });
+// 	var form = new Form(true);
+// 	console.log("form.gpu = ", form.gpu);
+// });
 
 // document.querySelector(".humans > button").addEventListener("click", function() {
-// 	location.replace("./humans.html");
-// 	});
+// 	var form = new Form(false);
+// 	console.log("form.gpu = ", form.gpu);
+// });
