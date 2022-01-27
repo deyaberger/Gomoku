33073c48bb12cd942ff70f35a99e8b9d1f60429f
Deya Berger <deyaberger@MacBook-Air-de-Deya.local>
2022-01-27 21:59:41 +0100

    omg im horny its so beautifuuul

--- src/index.js
+++ src/index.js
@@ -25,7 +25,41 @@ ws.onOpen = function() {
 
 
 ws.onError = function() {
-	alert();
+	window.alert = function(msg, callback){
+		$('.message').text(msg);
+		$('.customAlert').css('animation', 'fadeIn 0.3s linear');
+		$('.customAlert').css('display', 'inline');
+		setTimeout(function(){
+		  $('.customAlert').css('animation', 'none');
+		}, 300);
+		currentCallback = callback;
+	  }
+	  
+	$(function(){
+  
+		// add listener for when our confirmation button is clicked
+		  $('.confirmButton').click(function(){
+		  $('.customAlert').css('animation', 'fadeOut 0.3s linear');
+		  setTimeout(function(){
+		   $('.customAlert').css('animation', 'none');
+			  $('.customAlert').css('display', 'none');
+		  }, 300);
+		  currentCallback();
+		})
+		
+		$('.rab').click(function(){
+		  alert("If you think about anything, you are actually doing a recursive function which resolves your neurons into a deep pi calculation. You are then executing about 4294 threads in your brain, which do a parallel computation.", function(){
+			console.log("Callback executed");
+		  })
+		});
+		
+		// our custom alert box
+		setTimeout(function(){
+		  alert('Hi sweetheart, you are probably reading this alert box and have no clue why the heck you are even reading it, well guess what, the moon is actually just a big rock and we are all going to be sucked up by a black hole at some point. But maybe start the f**king server and refresh the page', function(){
+			  console.log("Callback executed");
+			});
+		}, 500);
+	  });
 	console.log("error");
 }
 
@@ -33,38 +67,5 @@ ws.onError = function() {
 var currentCallback;
 
 // override default browser alert
-window.alert = function(msg, callback){
-  $('.message').text(msg);
-  $('.customAlert').css('animation', 'fadeIn 0.3s linear');
-  $('.customAlert').css('display', 'inline');
-  setTimeout(function(){
-    $('.customAlert').css('animation', 'none');
-  }, 300);
-  currentCallback = callback;
-}
 
-$(function(){
-  
-  // add listener for when our confirmation button is clicked
-	$('.confirmButton').click(function(){
-    $('.customAlert').css('animation', 'fadeOut 0.3s linear');
-    setTimeout(function(){
-     $('.customAlert').css('animation', 'none');
-		$('.customAlert').css('display', 'none');
-    }, 300);
-    currentCallback();
-  })
-  
-  $('.rab').click(function(){
-    alert("If you think about anything, you are actually doing a recursive function which resolves your neurons into a deep pi calculation. You are then executing about 4294 threads in your brain, which do a parallel computation.", function(){
-      console.log("Callback executed");
-    })
-  });
-  
-  // our custom alert box
-  setTimeout(function(){
-    alert('Hi sweetheart, you are probably reading this alert box and have no clue why the heck you are even reading it, well guess what, the moon is actually just a big rock and we are all going to be sucked up by a black hole at some point. But maybe start the f**king server and refresh the page', function(){
-        console.log("Callback executed");
-      });
-  }, 500);
-});
+
