2c62b9cadd0dc95aa8ddb875ffa9b5527d725510
Deya Berger <deyaberger@MacBook-Air-de-Deya.local>
2022-01-27 16:39:57 +0100

    bonne gestion form

--- src/form/form.js
+++ src/form/form.js
@@ -2,37 +2,54 @@ import "./form.css";
 
 console.log("form");
 
-var ws = sessionStorage.getItem("websocket");
-console.log(ws);
-ws = JSON.parse(ws);
-console.log(ws.url);
-
-// document.getElementsByTagName("a").addEventListener("click", function() {
-// 	console.log("form.gpu = ", form.gpu);
-// });
+// var ws = sessionStorage.getItem("websocket");
+// ws = JSON.parse(ws);
+// console.log(ws.url);
 
 
-
-// var input_list = document.querySelectorAll(".user-box > input");
-// console.log(input_list);
-
-// var gpu = false;
-
-// if (input_list)
-// {
-// 	input_list[0].addEventListener("click", function() {
-// 		// 	var form = new Form(false);
-// 		// 	console.log("form.gpu = ", form.gpu);
-// 		});
-// }
-
-if (document.title == 'Computer')
-{
-	
-	console.log("You want to play against the computer");
-}
-else if (document.title == 'Humans')
-{
-	console.log("You want to play against a human");
+export default class Form {
+	constructor() {
+		this.gpu = true;
+		this.player1 = "black";
+		this.player2 = "white";
+	}
 }
 
+let form = new Form();
+
+var click_start = document.getElementsByTagName("a")[0];
+var html_form = document.getElementById('my_form');
+console.log(html_form);
+
+if (click_start){
+	click_start.addEventListener("click", function () {start()});
+}
+
+if (html_form){
+	function handleForm(event) {
+		event.preventDefault();
+		start();
+		location.href = "./game.html";
+	} 
+	html_form.addEventListener('submit', handleForm);
+}
+
+
+function start() {
+	var input_list = document.querySelectorAll(".user-box > input");
+	if (input_list && input_list[0].value != "")
+	{
+		form.player1 = input_list[0].value;
+	}
+	if (input_list && input_list[1] && input_list[1].value != "")
+	{
+		form.player2 = input_list[1].value;
+		form.gpu = false;
+	}
+	else if (input_list && input_list[0].value != "" && !input_list[1])
+	{
+		form.player2 = "computer";
+	}
+	sessionStorage.setItem("inputs", JSON.stringify(input_list));
+	sessionStorage.setItem("form", JSON.stringify(form));
+}
--- src/game/game.js
+++ src/game/game.js
@@ -1,26 +1,36 @@
 import "./game.css";
-import Ws from "../Communicate.js";
 import GameView from "./GameView.js";
 
 console.log("game");
 
+var ws = sessionStorage.getItem("websocket");
+ws = JSON.parse(ws);
+var form = sessionStorage.getItem("form");
+form = JSON.parse(form);
 
-let gameview = new GameView(document.getElementById("app"));
+var inputs = sessionStorage.getItem("inputs");
+inputs = JSON.parse(inputs);
 
-let ws = new Ws();
-ws.new_connection(gameview);
+console.log(ws.url);
+console.log(form);
+console.log(inputs);
+
+// let gameview = new GameView(document.getElementById("app"));
+
+// let ws = new Ws();
+// ws.new_connection(gameview);
 
 
-gameview.onInterClick = function(i, j) {
-	console.log("just clicked on intersection", i , "x", j);
-	gameview.makeMove(i, j);
-	ws.send_move(i, j);
-}
+// gameview.onInterClick = function(i, j) {
+// 	console.log("just clicked on intersection", i , "x", j);
+// 	gameview.makeMove(i, j);
+// 	ws.send_move(i, j);
+// }
 
-gameview.onRestartClick = function() {
-	console.log("just clicked on restart");
-	gameview.restart();
-	ws.restart(gameview);
-}
+// gameview.onRestartClick = function() {
+// 	console.log("just clicked on restart");
+// 	gameview.restart();
+// 	ws.restart(gameview);
+// }
 
 
