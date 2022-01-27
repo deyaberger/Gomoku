f96a5d9674170f7bf3c63aad458698088f1502e0
Deya Berger <deyaberger@MacBook-Air-de-Deya.local>
2022-01-27 21:10:51 +0100

    avec Keypress added

--- src/form/form.js
+++ src/form/form.js
@@ -1,10 +1,5 @@
 import "./form.css";
 
-// Avoid Implicit submission when pressing Enter
-$(document).on("keydown", "form", function(event) { 
-    return event.key != "Enter";
-});
-
 // Class form that will help to comunicate with the server
 export default class Form {
 	constructor() {
@@ -24,13 +19,35 @@ export default class Form {
 
 let form = new Form();
 
+// Make sur we can send the form also by clicking on the save button
 var click_start = document.getElementsByTagName("a")[0];
 
 if (click_start){
 	click_start.addEventListener("click", function () {start()});
 }
 
-// When button start has been clicked, we keep in session storage all the inputs
+// Make sur we can send the form also by pressing Enter
+var player1 = document.getElementById("player1");
+if (player1) {
+player1.addEventListener("keypress", function(event) {
+  if (event.key === 'Enter') {
+	event.preventDefault();
+	click_start.click();
+  }
+});
+}
+
+var player2 = document.getElementById("player2");
+if (player2) {
+player2.addEventListener("keypress", function(event) {
+  if (event.key === 'Enter') {
+	event.preventDefault();
+	click_start.click();
+  }
+});
+}
+
+// When button or Enter has been pressed, we keep in session storage all the inputs
 function start() {
 	var input_list = document.querySelectorAll(".user-box > input");
 	if (input_list && input_list[0].value != "")
