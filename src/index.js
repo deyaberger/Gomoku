5573bc32508748010c520babe239d3b72fe399bd
Deya Berger <bergerdeya@gmail.com>
2022-02-14 16:03:19 +0100

    tryiing to hide ugly flash

--- src/index.js
+++ src/index.js
@@ -51,6 +51,16 @@ $('.confirmButton').click(function(){
 
 var currentCallback;
 
-// override default browser alert
+// Helper function
+let domReady = (cb) => {
+	document.readyState === 'interactive' || document.readyState === 'complete'
+	  ? cb()
+	  : document.addEventListener('DOMContentLoaded', cb);
+  };
+  
+  domReady(() => {
+	// Display body when DOM is loaded
+	document.body.style.visibility = 'visible';
+  });
 
 
